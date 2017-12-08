6648034df39d80145ce20aeb4422223c8c8b6353
Matt Day <mattsday@gmail.com>
2017-12-08 12:19:08 +0000

    Updated to use service broker

--- src/main/java/io/pivotal/mday/azure/analysisdemo/config/AzureTextAnalysisConfig.java
+++ src/main/java/io/pivotal/mday/azure/analysisdemo/config/AzureTextAnalysisConfig.java
@@ -5,18 +5,31 @@
  */
 package io.pivotal.mday.azure.analysisdemo.config;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AzureTextAnalysisConfig {
 
-	@Value("${azure.text-analysis.uri}")
 	private String uri;
-	@Value("${azure.text-analysis.subscription-key}")
 	private String apiKey;
 	@Value("${azure.text-analysis.language}")
 	private String language;
+
+	public AzureTextAnalysisConfig() {
+		final String env = System.getenv("VCAP_SERVICES");
+		try {
+			// Pull out the first credentials in VCAP_SERVICES
+			JSONObject credentialJson = new JSONObject(env).getJSONArray("azure-cognitive-text").getJSONObject(0)
+					.getJSONObject("credentials");
+
+			this.uri = credentialJson.getString("uri");
+			this.apiKey = credentialJson.getString("secret");
+		} catch (Exception e) {
+			throw new RuntimeException("Can't get VCAP Services - is there a service instance bound?");
+		}
+	}
 
 	public String getUri() {
 		return uri;
